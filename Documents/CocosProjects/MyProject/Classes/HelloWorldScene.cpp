650c46aeb619860a961068fecada9f25077b59e4
dingyipeng <957312749@qq.com>
2015-04-10 14:49:54 +0800

    commit

---
+++ Documents/CocosProjects/MyProject/Classes/HelloWorldScene.cpp
@@ -0,0 +1,46 @@
+#include "HelloWorldScene.h"
+#include "cocostudio/CocoStudio.h"
+#include "ui/CocosGUI.h"
+
+USING_NS_CC;
+
+using namespace cocostudio::timeline;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Sprite * sprite =Sprite::create("hero1.png");
+    sprite->setPosition(Vec2(100,400));
+    addChild(sprite,1);
+    
+    Sprite * background =Sprite::create("bg.png");
+    background->setAnchorPoint(Vec2(0,0));
+    addChild(background,0);
+    return true;
+}
+void HelloWorld::update(float dt)
+{
+    
+}
